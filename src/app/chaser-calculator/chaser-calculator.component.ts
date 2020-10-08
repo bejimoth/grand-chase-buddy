5761fc1e1f05be67c729dfa3fa1334c6ff6fcc7c
bejimoth <lorenzo.cea.ko@gmail.com>
2020-10-07 22:03:44 -0300

    first chaser-calculator prototype

--- src/app/chaser-calculator/chaser-calculator.component.ts
+++ src/app/chaser-calculator/chaser-calculator.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChaserCalculatorComponent implements OnInit {
 
+  title: string = 'Chaser Crystals Calculator';
+  currentLevel: number;
+  targetLevel: number;
+  materials: number;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  calculate(): void {
+    this.materials = 0;
+    let i = this.currentLevel ? this.currentLevel : 0;
+    let sum = 0;
+    
+    for (i; i < this.targetLevel; i++) {
+      if (i < 5) {
+        sum = 40;
+      } else if (i < 10) {
+        sum = 76;
+      } else if (i < 15) {
+        sum = 144; //TODO: I need to get the actual value
+      } else if (i < 20) {
+        sum = 238;
+      } else if (i < 25) {
+        sum = 364;
+      }
+      
+      this.materials += sum;
+    }
+  }
 }
